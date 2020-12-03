6941a16f5fc9fbb579978ddb1e412af985044c64
edinsonrequena <edinsonrequena@protonmail.com>
2020-12-03 15:18:46 -0400

    update posts app

--- clonigram/posts/apps.py
+++ clonigram/posts/apps.py
@@ -1,5 +1,13 @@
+'''
+Post application module.
+'''
 from django.apps import AppConfig
 
 
 class PostsConfig(AppConfig):
+    '''
+    :type name: str
+    type verbose_name: str
+    '''
     name = 'posts'
+    verbose_name = 'Posts'
--- clonigram/posts/views.py
+++ clonigram/posts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+'''
+Posts views
+'''
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+

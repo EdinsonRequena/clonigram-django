98a1505b010422596250e016f4a81ee17ffd2572
edinsonrequena <edinsonrequena@protonmail.com>
2020-12-03 12:53:27 -0400

    update views.py

--- clonigram/clonigram/views.py
+++ clonigram/clonigram/views.py
@@ -2,12 +2,29 @@
 
 # Django modules
 from django.http import HttpResponse
+import json
+
 
 # Utilities
 from datetime import datetime as dt
 
 def hello_world(request):
+    ''' Return a Gretting'''
     hour = dt.now().strftime('%b %dth, %Y - %H:%M hrs')
+
     return HttpResponse(f'Hello, the server time is: {hour}')
 
 
+def hi(request):
+    """HI"""
+    numbers = [int(i) for i in request.GET['numbers'].split(',')]
+    sort_int = sorted(numbers)
+    data = {
+        'status': 'ok',
+        'numbers': sort_int,
+        'msg': 'Integers sorted succesfully'
+    }
+    return HttpResponse(
+        json.dumps(data, indent=4),
+        content_type='application/json'
+        )

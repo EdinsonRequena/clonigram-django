4fc9badaf402f91b7cc3452d5db0338068a57d35
edinsonrequena <edinsonrequena@protonmail.com>
2020-12-05 13:24:55 -0400

    adding user models

--- clonigram/posts/models.py
+++ clonigram/posts/models.py
@@ -1,3 +1,30 @@
+'''
+Post Models.
+'''
+
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    '''
+    :type email: EmailField
+    :type password: CharField
+    :type first_name: CharField
+    :type last_name: CharField
+    :type bio: CharField
+    :type birthdate: DateField
+    :type created: DateTimeField
+    :type modified: DateTimeField
+    '''
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    bio = models.TextField(blank=True)
+
+    birthdate = models.DateField(blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)

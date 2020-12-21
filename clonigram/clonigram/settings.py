5c9b7ad9d0d36e2eb4107d6d5392ebfc0b8d70ef
edinsonrequena <edinsonrequena@protonmail.com>
2020-12-21 14:38:53 -0400

    update settings.y

--- clonigram/clonigram/settings.py
+++ clonigram/clonigram/settings.py
@@ -131,6 +131,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'

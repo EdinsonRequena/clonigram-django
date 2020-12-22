e1f55fe658e1de4bb2966a3fb8f2b5f9ab804892
edinsonrequena <edinsonrequena@protonmail.com>
2020-12-22 15:02:48 -0400

    update views.py

--- clonigram/users/views.py
+++ clonigram/users/views.py
@@ -1,3 +1,12 @@
+"""
+    Users Views
+"""
+
+# Django dependecies
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 
-# Create your views here.
+def login_view(request):
+    """Login view.
+    """
+    return render(request, 'users/login.html')

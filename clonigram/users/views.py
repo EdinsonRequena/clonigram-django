27f7b7964a5f227bc0b63d1cfd8836787192c81f
edinsonrequena <edinsonrequena@protonmail.com>
2021-01-12 16:38:59 -0400

    update views

--- clonigram/users/views.py
+++ clonigram/users/views.py
@@ -7,6 +7,13 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+# Execption dependencies
+from django.db.utils import IntegrityError
+
+# Models dependencies
+from django.contrib.auth.models import User
+from users.models import Profile
+
 def login_view(request):
     """Login view.
     """
@@ -26,6 +33,29 @@ def login_view(request):
 def signup(request):
     """Sign up view.
     """
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirm = request.POST['passwd_confirm']
+
+        if passwd != passwd_confirm:
+            return render(request, 'users/signup.html', {'error': "Password and Password confirmation doesn't match"})
+
+        try:
+            user = User.objects.create_user(username=username, password=passwd)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': "Username alredy exists"})
+
+        user.firts_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
 
 
 @login_required

194bde69a85dbee6c1931b15580024b4e5280248
edinsonrequena <edinsonrequena@protonmail.com>
2021-01-13 13:06:04 -0400

    update urls.py

--- clonigram/clonigram/urls.py
+++ clonigram/clonigram/urls.py
@@ -38,6 +38,7 @@ urlpatterns = [
     #URLs at users_views
     path('users/login/', users_views.login_view, name='login'),
     path('users/logout/', users_views.logout_view, name='logout'),
-    path('users/signup/', users_views.signup, name='signup')
+    path('users/signup/', users_views.signup, name='signup'),
+    path('users/me/profile/', users_views.update_profile, name='update_profile'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

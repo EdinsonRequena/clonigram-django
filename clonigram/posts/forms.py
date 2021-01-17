2f0506a77716928c5a8b025ec67c153ed9f7a17b
edinsonrequena <edinsonrequena@protonmail.com>
2021-01-17 15:36:55 -0400

    update

---
+++ clonigram/posts/forms.py
@@ -0,0 +1,19 @@
+""" Forms at Posts
+"""
+
+# Django Modules
+from django import forms
+
+# Posts Modules
+from posts.models import Post
+
+class PostForm(forms.ModelForm):
+    """Posts model form.
+    """
+
+    class Meta:
+        """form settings
+        """
+
+        model = Post
+        fields = ('user', 'profile', 'title', 'photo')

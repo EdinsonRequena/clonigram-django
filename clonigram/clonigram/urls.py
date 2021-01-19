5726100ca052bcac1f69e00720c12948f58bd256
edinsonrequena <edinsonrequena@protonmail.com>
2021-01-19 14:26:55 -0400

    update urls at clonigram

--- clonigram/clonigram/urls.py
+++ clonigram/clonigram/urls.py
@@ -18,29 +18,16 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
-# Clonigram Modules
-from clonigram import  views as local_views
-# Posts Modules
-from posts import views as posts_views
-# Users Modules
-from users import views as users_views
+from django.urls import path, include
+
 
 urlpatterns = [
-    # URLs at local_views
+    # URLs at admin
     path('admin/', admin.site.urls),
-    path('hello-world/', local_views.hello_world, name='Hello_world'),
-    path('sorted/', local_views.sorted_intengers, name='sorted'),
-    path('hi/<str:name>/<int:age>', local_views.say_hi, name='hi'),
+    # URLs at posts
+    path('', include(('posts.urls', 'posts'), namespace='posts')),
+    #URLs at users
+    path('users/', include(('users.urls', 'users'), namespace='users')),
 
-    # URLs at posts_views
-    path('', posts_views.list_posts, name='feed'), # Localhost:8000
-    path('posts/new', posts_views.create_post, name='create_post'),
-
-    #URLs at users_views
-    path('users/login/', users_views.login_view, name='login'),
-    path('users/logout/', users_views.logout_view, name='logout'),
-    path('users/signup/', users_views.signup, name='signup'),
-    path('users/me/profile/', users_views.update_profile, name='update_profile'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

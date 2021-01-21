455ea4e287ecdb90a6f96c2b003aaaeba8339dea
edinsonrequena <edinsonrequena@protonmail.com>
2021-01-21 11:50:44 -0400

    update views.py

--- clonigram/posts/views.py
+++ clonigram/posts/views.py
@@ -9,37 +9,6 @@ from django.shortcuts import render, redirect
 from posts.forms import PostForm
 from posts.models import Post
 
-
-posts = [
-    {
-        'title': 'Mont Blanc',
-        'user': {
-            'name': 'Andrea Viñas',
-            'picture': 'https://picsum.photos/60/60/?image=1027'
-        },
-        'timestamp': dt.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/800/600?image=1036',
-    },
-    {
-        'title': 'Via Láctea',
-        'user': {
-            'name': 'Edinson Requena',
-            'picture': 'https://picsum.photos/60/60/?image=1005'
-        },
-        'timestamp': dt.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/800/800/?image=903',
-    },
-    {
-        'title': 'Nuevo auditorio',
-        'user': {
-            'name': 'Uriel (thespianartist)',
-            'picture': 'https://picsum.photos/60/60/?image=883'
-        },
-        'timestamp': dt.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/500/700/?image=1076',
-    }
-]
-
 @login_required
 def list_posts(request):
     '''
@@ -49,6 +18,7 @@ def list_posts(request):
     posts = Post.Objects.all().order_by('-created')
 
     return render(request, 'posts/feed.html', {'posts': posts})
+
 
 @login_required
 def create_post(request):

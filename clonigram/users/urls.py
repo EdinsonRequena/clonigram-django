17a2b5617e469d97a03d58abb07f91099747db81
edinsonrequena <edinsonrequena@protonmail.com>
2021-01-21 13:38:45 -0400

    update

--- clonigram/users/urls.py
+++ clonigram/users/urls.py
@@ -2,7 +2,6 @@
 """
 # Django Modules.
 from django.urls import path
-from django.views.generic import TemplateView
 # Users Modules.
 from users import views
 
@@ -11,28 +10,28 @@ urlpatterns = [
     # Posts
     path(
         route='<str:username>/',
-        view=TemplateView.as_view(template_name='users/detail'),
+        view=views.UserDetailView.as_view(),
         name='detail'
     ),
 
     # Managment
     path(
-        route='users/login/',
+        route='login/',
         view=views.login_view,
         name='login'
         ),
     path(
-        route='users/logout/',
+        route='logout/',
         view=views.logout_view,
         name='logout'
     ),
     path(
-        route='users/signup/',
+        route='signup/',
         view=views.signup, name='signup'
         ),
     path(
-        route='users/me/profile/',
+        route='me/profile/',
         view=views.update_profile,
-        name='update_profile'
+        name='update'
     ),
 ]
